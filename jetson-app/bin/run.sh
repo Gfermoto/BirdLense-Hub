0902955e44dd81b7e21c8700274d99ec5edf0c09
alex <aleksandr.rogachev1994@gmail.com>
2024-07-05 17:03:50 +0000

    exploring docker compose
    Former-commit-id: f370853fcd22fe779259ec632ada1fc3697863c3

--- jetson-app/bin/run.sh
+++ jetson-app/bin/run.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 # Change to the expected directory
-cd ~/Documents/jetson-inference
-
-docker/run.sh -v ~/Documents/experiment:/jetson-inference/app --run python3 app/main.py
-# docker/run.sh -v ~/Documents/experiment:/jetson-inference/app
+sudo docker run --runtime nvidia -it --rm \
+		--network host \
+		-v /tmp/argus_socket:/tmp/argus_socket \
+		-v /etc/enctune.conf:/etc/enctune.conf \
+		-v /etc/nv_tegra_release:/etc/nv_tegra_release \
+		-v /tmp/nv_jetson_model:/tmp/nv_jetson_model \
+		-v /var/run/dbus:/var/run/dbus \
+		-v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket \
+		-w $DOCKER_ROOT \
+		$DISPLAY_DEVICE $V4L2_DEVICES \
+		$DATA_VOLUME $USER_VOLUME $DEV_VOLUME \
+		$CONTAINER_IMAGE $USER_COMMAND
