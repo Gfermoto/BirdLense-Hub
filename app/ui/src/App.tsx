2c9ba3d56653021fab311b45436bb704e8673a07
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2024-11-22 11:46:46 -0500

    clean up css, material icons, robot font
    Former-commit-id: 44c02608c0b15d094df226f5449e746163ead806

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { Navigation } from './components/Navigation';
 import { HomePage } from './pages/HomePage';
 import { FoodManagement } from './pages/FoodManagement';
@@ -20,17 +20,16 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
-        <div className="min-h-screen bg-gray-50">
           <Navigation />
-          <main className="container mx-auto px-4 py-8">
+          <main>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/food" element={<FoodManagement />} />
               <Route path="/birds" element={<BirdDirectory />} />
             </Routes>
           </main>
-        </div>
       </BrowserRouter>
     </ThemeProvider>
   );
